09b350d4941be31cb9a098e72f5c7e4474314c4c
Shruti <75881545+Shrutiswift@users.noreply.github.com>
2023-01-19 20:15:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (event)=>{
+    if(event.target.innerHTML == '='){
+      string = eval(string);
+      console.log(string);
+      document.querySelector('input').value = string;
+
+    }else{
+    console.log(event.target);
+    string = string + event.target.innerHTML;
+    document.querySelector('input').value = string;
+      
+    }
+  })
+})
